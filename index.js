c207ddaa9c84931bbd7ee2e4836d135c0286f0c8
Charles Eul-Stacy <myherokelly@gmail.com>
2018-04-02 03:59:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ function addToCart(item) {
  }
 
  cart.push(product);
- console.log(`${item} has been added to your cart.`);
- return cart;
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
