f8a4e414394462eebfc3c2e70809511e0e15f73a
Charles Eul-Stacy <myherokelly@gmail.com>
2018-04-02 04:07:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++){
     if(cart[i].itemName === item){
       return cart.splice(i,1);
-    } 
+    }
   }
   return `That item is not in your cart.`;
 }
